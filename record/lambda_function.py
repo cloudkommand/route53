fdca5be7a734121056a2e4ef21f2c7d9989745cd
Azra Eiden <carter.eiden@gmail.com>
2023-02-20 00:46:15 -0500

    Allowing passing a hosted zone ID

--- record/lambda_function.py
+++ record/lambda_function.py
@@ -42,14 +42,28 @@ def lambda_handler(event, context):
         domain = cdef.get("domain") or cdef.get("target_s3_bucket") or \
             form_domain(component_safe_name(project_code, repo_id, cname, no_underscores=False), cdef.get("base_domain"))
             # form_domain(, cdef.get("base_domain")) or \
+        route53_hosted_zone_id = cdef.get("route53_hosted_zone_id")
+        prior_route53_hosted_zone_id = prev_state.get("props", {}).get("hosted_zone_id")
+        if prior_route53_hosted_zone_id and route53_hosted_zone_id and (prior_route53_hosted_zone_id != route53_hosted_zone_id):
+            eh.add_log("Cannot Change Hosted Zone ID", {"prior": prior_route53_hosted_zone_id, "new": route53_hosted_zone_id}, is_error=True)
+            eh.perm_error("Cannot Change Hosted Zone ID", 0)
+            return eh.finish()
 
+        hosted_zone_id = prior_route53_hosted_zone_id or route53_hosted_zone_id
+
+        #Should store in props
         record_type = cdef.get("record_type") or "A"
 
         pass_back_data = event.get("pass_back_data", {})
-        if not pass_back_data:
-            eh.add_op("manage_record_set")
+        if pass_back_data:
+            pass
+        else:
+            eh.add_op("get_hosted_zone")
+            eh.add_op("get_record_set")
 
-        manage_record_set(prev_state, cdef, event.get("op"), domain, record_type)
+
+        get_hosted_zone(hosted_zone_id, domain, record_type)
+        get_record_set(prev_state, cdef, event.get("op"), domain, record_type)
         update_record_set(domain, record_type)
         check_update_complete()
         
@@ -63,37 +77,98 @@ def lambda_handler(event, context):
         return eh.finish()
 
 
-@ext(handler=eh, op="manage_record_set")
-def manage_record_set(prev_state, cdef, op, domain, record_type):
+@ext(handler=eh, op="get_hosted_zone")
+def get_hosted_zone(hosted_zone_id, domain, record_type):
+    if hosted_zone_id:
+        try:
+            old_zone = route53.get_hosted_zone(Id=hosted_zone_id).get("HostedZone")
+            eh.add_log("Found Hosted Zone", {"response": old_zone})
+            eh.add_props({"hosted_zone_id": hosted_zone_id})
+            # eh.add_op("get_record_set", {"zone": old_zone.get("HostedZone"), "domain": domain, "record_type": record_type})
+
+        except ClientError as e:
+            handle_common_errors(e, eh, "Failed to Get Hosted Zone", 10, ["NoSuchHostedZone", "InvalidInput"])
+    
+    else:
+        hosted_zone_response = route53.list_hosted_zones()
+        print(f"hosted_zone_response = {hosted_zone_response}")
+        for zone in hosted_zone_response.get("HostedZones"):
+            if zone.get("Config", {}).get("PrivateZone"):
+                continue
+            elif domain.endswith(zone['Name'][:-1]):
+                old_zone = zone
+                break
+
+        print(f"old_zone = {old_zone}")
+        if not old_zone and hosted_zone_response.get("IsTruncated"):
+            marker = hosted_zone_response.get("NextMarker")
+            while marker and not old_zone:
+                hosted_zone_response = route53.list_hosted_zones(Marker=marker)
+                for zone in hosted_zone_response.get("HostedZones"):
+                    if zone.get("Config", {}).get("PrivateZone"):
+                        continue
+                    elif domain.endswith(zone['Name'][-1]):
+                        old_zone = zone
+                        break
+                marker = hosted_zone_response.get("NextMarker") if hosted_zone_response.get("IsTruncated") else None
+
+        if not old_zone:
+            eh.add_log("No Hosted Zone Found", {"response": hosted_zone_response, "domain": domain})
+            eh.perm_error("No Hosted Zone Found")
+        else:
+            eh.add_props({"hosted_zone_id": old_zone.get("Id")})
+
+
+
+@ext(handler=eh, op="get_record_set")
+def get_record_set(prev_state, cdef, op, domain, record_type):
     # route53 = boto3.client("route53")
-    if not domain:
-        eh.perm_error("No Domain")
-        eh.add_log("No Domain or S3 Bucket", {"component_definition": cdef}, True)
-        return 0
-    identifier = cdef.get("identifier")
-    weight = cdef.get("weight")
-    region = cdef.get("region")
-    continent = cdef.get("continent")
-    state = cdef.get("state")
-    country = cdef.get("country") if not state else "US"
-    failover = cdef.get("failover")
-    multivalue_answer = cdef.get("multivalue_answer")
-    ttl = cdef.get("ttl")
-    resource_records = cdef.get("resource_records")
-    s3_region = cdef.get("target_s3_region")
-    cloudfront_domain_name = cdef.get("target_cloudfront_domain_name")
-    api_hosted_zone_id = cdef.get("target_api_hosted_zone_id")
-    api_domain_name = cdef.get("target_api_domain_name")
-    evaluate_health = cdef.get("evaluate_target_health") or False
+    # if not domain:
+    #     eh.perm_error("No Domain")
+    #     eh.add_log("No Domain or S3 Bucket", {"component_definition": cdef}, True)
+    #     return 0
 
-    old_domain = prev_state.get("props", {}).get("domain") if op == "upsert" else domain
-    remove_set, remove_zone = None, None
-    if old_domain and ((old_domain != domain) or op == "delete"):
-        remove_set, remove_zone = get_set(old_domain, record_type)
+    hosted_zone_id = eh.props["hosted_zone_id"]
 
-    upsert_set, current_zone = None, None
-    if op == "upsert":
-        current_set, current_zone = get_set(domain, record_type)
+    response = route53.list_resource_record_sets(
+        HostedZoneId=hosted_zone_id,
+        StartRecordName=domain
+    )
+    current_set = None
+    print(f"Record Set Response {response}")
+    for record_set in response.get("ResourceRecordSets"):
+        if record_set.get("Name")[:-1] == domain and record_set.get("Type") == record_type:
+            current_set = record_set
+            current_set['Name'] = current_set['Name'][:-1]
+            if current_set.get("AliasTarget").get("DNSName"):
+                current_set['AliasTarget']['DNSName'] = current_set['AliasTarget']['DNSName'][:-1]
+            eh.add_log("Found Matching Record Set", {"record_set": record_set})
+            break
+
+    if op == "delete":
+        if current_set:
+            eh.add_op("update_record_set", {"remove": current_set})
+        else:
+            eh.add_log("No Record Set to Delete", {"domain": domain, "record_type": record_type})
+
+    else:
+        identifier = cdef.get("identifier")
+        weight = cdef.get("weight")
+        region = cdef.get("region")
+        continent = cdef.get("continent")
+        state = cdef.get("state")
+        country = cdef.get("country") if not state else "US"
+        failover = cdef.get("failover")
+        multivalue_answer = cdef.get("multivalue_answer")
+        ttl = cdef.get("ttl")
+        resource_records = cdef.get("resource_records")
+        s3_region = cdef.get("target_s3_region")
+        cloudfront_domain_name = cdef.get("target_cloudfront_domain_name")
+        api_hosted_zone_id = cdef.get("target_api_hosted_zone_id")
+        api_domain_name = cdef.get("target_api_domain_name")
+        evaluate_health = cdef.get("evaluate_target_health") or False
+
+        update_record_set_op_val = {}
         if api_hosted_zone_id and api_domain_name:
             hosted_zone_id = api_hosted_zone_id
             domain_name = api_domain_name
@@ -132,20 +207,55 @@ def manage_record_set(prev_state, cdef, op, domain, record_type):
         print(f"current_set = {current_set}")
         print(f"desired_set = {desired_set}")
         if current_set != desired_set:
-            upsert_set = desired_set
+            update_record_set_op_val = {"upsert": current_set}
+        
+        old_domain = prev_state.get("props", {}).get("domain")
+        if old_domain and old_domain != domain:
+            old_record_type = prev_state.get("props", {}).get("record_type")
+            old_set = None
+            response = route53.list_resource_record_sets(
+                HostedZoneId=hosted_zone_id,
+                StartRecordName=old_domain
+            )
+
+            for record_set in response.get("ResourceRecordSets"):
+                if record_set.get("Name")[:-1] == old_domain and record_set.get("Type") == old_record_type:
+                    old_set = record_set
+                    old_set['Name'] = old_set['Name'][:-1]
+                    if old_set.get("AliasTarget").get("DNSName"):
+                        old_set['AliasTarget']['DNSName'] = old_set['AliasTarget']['DNSName'][:-1]
+                    eh.add_log("Found Set to Remove", {"record_set": record_set})
+                    update_record_set_op_val["remove"] = old_set
+                    break
+
+        if update_record_set_op_val:
+            eh.add_op("update_record_set", update_record_set_op_val)
         else:
             eh.add_log("No Records to Write", {"current_set": current_set})
-            eh.add_props({"domain": domain, "hosted_zone_id": current_zone['Id'], "record_type": record_type})
+            eh.add_props({"domain": domain, "hosted_zone_id": hosted_zone_id, "record_type": record_type})
             eh.add_links({
-                "Record Set": gen_route53_link(current_zone['Id']),
+                "Record Set": gen_route53_link(hosted_zone_id),
                 "Domain URL": f"https://{domain}"
             })
 
-    if remove_set or upsert_set:
-        eh.add_op("update_record_set", {
-            "remove": remove_none_attributes({"set": remove_set, "zone": remove_zone}), 
-            "upsert": remove_none_attributes({"set": upsert_set, "zone": current_zone})
-            })
+    # old_domain = prev_state.get("props", {}).get("domain") if op == "upsert" else domain
+    # remove_set, remove_zone = None, None
+    # if old_domain and ((old_domain != domain) or op == "delete"):
+    #     remove_set, remove_zone = get_set(old_domain, record_type)
+
+    # upsert_set, current_zone = None, None
+    # if op == "upsert":
+    #     current_set, current_zone = get_set(domain, record_type)
+        
+    #     else:
+    #         eh.add_log("No Records to Write", {"current_set": current_set})
+            
+
+    # if remove_set or upsert_set:
+    #     eh.add_op("update_record_set", {
+    #         "remove": remove_none_attributes({"set": remove_set, "zone": remove_zone}), 
+    #         "upsert": remove_none_attributes({"set": upsert_set, "zone": current_zone})
+    #         })
 
 
 @ext(handler=eh, op="update_record_set")
@@ -153,44 +263,51 @@ def update_record_set(domain, record_type):
     remove = eh.ops['update_record_set'].get("remove")
     upsert = eh.ops['update_record_set'].get("upsert")
 
-    zone_1_id = upsert.get("zone", {}).get('Id')
-    zone_2_id = remove.get("zone", {}).get('Id')
+    zone_id = eh.props.get("hosted_zone_id")
 
-    if zone_1_id and zone_2_id and zone_1_id != zone_2_id:
-        params1 = gen_params(zone_1_id, upsert.get("set"))
-        change_id_1 = run_update(params1)
-        params2 = gen_params(zone_2_id, remove.get("set"))
-        change_id_2 = run_update(params2)
-        eh.add_op("check_update_complete", [change_id_1, change_id_2])
+    # zone_1_id = upsert.get("zone", {}).get('Id')
+    # zone_2_id = remove.get("zone", {}).get('Id')
 
-    else:
-        zone_id = zone_1_id or zone_2_id
-        changes = []
-        if zone_1_id:
-            changes.append({
-                "Action": "UPSERT",
-                "ResourceRecordSet": upsert.get("set")
-            })
+    # if zone_1_id and zone_2_id and zone_1_id != zone_2_id:
+    #     params1 = gen_params(zone_1_id, upsert.get("set"))
+    #     change_id_1 = run_update(params1)
+    #     params2 = gen_params(zone_2_id, remove.get("set"))
+    #     change_id_2 = run_update(params2)
+    #     eh.add_op("check_update_complete", [change_id_1, change_id_2])
 
-        if zone_2_id:
-            changes.append({
-                "Action": "DELETE",
-                "ResourceRecordSet": remove.get("set")
-            })
+    changes = []
+    if upsert:
+        changes.append({
+            "Action": "UPSERT",
+            "ResourceRecordSet": upsert.get("set")
+        })
 
-        params = {
-            "HostedZoneId": zone_id,
-            "ChangeBatch": {
-                "Comment":  "Change Made By CloudKommand",
-                "Changes": changes
-            }
+    if remove:
+        changes.append({
+            "Action": "DELETE",
+            "ResourceRecordSet": remove.get("set")
+        })
+
+    params = {
+        "HostedZoneId": zone_id,
+        "ChangeBatch": {
+            "Comment":  "Change Made By CloudKommand",
+            "Changes": changes
         }
-        change_id = run_update(params)
-        eh.add_op("check_update_complete", [change_id])
+    }
+    try:
+        response = route53.change_resource_record_sets(**params)
+        eh.add_log("Updated Record(s)", {"response": response})
+        change_id = response.get("ChangeInfo")['Id']
+    except ClientError as e:
+        handle_common_errors(e, eh, "Failed to Update Record", 60, ["NoSuchHostedZone", "NoSuchHealthCheck", "InvalidChangeBatch", "InvalidInput"])
 
-    eh.add_props({"domain": domain, "hosted_zone_id": zone_1_id, "record_type": record_type})
+    # change_id = run_update(params)
+    eh.add_op("check_update_complete", [change_id])
+
+    eh.add_props({"domain": domain, "hosted_zone_id": zone_id, "record_type": record_type})
     eh.add_links({
-        "Record Set": gen_route53_link(zone_1_id),
+        "Record Set": gen_route53_link(zone_id),
         "Domain URL": f"https://{domain}"
     })
 
@@ -212,6 +329,7 @@ def check_update_complete():
     if not eh.error:
         eh.add_log("Record(s) Complete", {"response": response})
 
+
 def run_update(params):
     try:
         response = route53.change_resource_record_sets(**params)
@@ -219,6 +337,8 @@ def run_update(params):
         return response.get("ChangeInfo")['Id']
     except ClientError as e:
         handle_common_errors(e, eh, "Failed to Update Record", 60, ["NoSuchHostedZone", "NoSuchHealthCheck", "InvalidChangeBatch", "InvalidInput"])
+
+    
 
 def gen_params(zone_id, set_):
     return {
@@ -232,48 +352,40 @@ def gen_params(zone_id, set_):
             }
         }
 
-def get_set(domain, record_type):
-    # route53 = boto3.client("route53")
-    found_set = None
-    old_zone = None
-    hosted_zone_response = route53.list_hosted_zones()
-    print(f"hosted_zone_response = {hosted_zone_response}")
-    for zone in hosted_zone_response.get("HostedZones"):
-        if domain.endswith(zone['Name'][:-1]):
-            old_zone = zone
-            break
+# def get_set(domain, record_type):
+#     # route53 = boto3.client("route53")
+#     found_set = None
+#     old_zone = None
+#     hosted_zone_response = route53.list_hosted_zones()
+#     print(f"hosted_zone_response = {hosted_zone_response}")
+#     for zone in hosted_zone_response.get("HostedZones"):
+#         if domain.endswith(zone['Name'][:-1]):
+#             old_zone = zone
+#             break
 
-    print(f"old_zone = {old_zone}")
-    if not old_zone and hosted_zone_response.get("IsTruncated"):
-        marker = hosted_zone_response.get("NextMarker")
-        while marker and not old_zone:
-            hosted_zone_response = route53.list_hosted_zones(Marker=marker)
-            for zone in hosted_zone_response.get("HostedZones"):
-                if domain.endswith(zone['Name'][-1]):
-                    old_zone = zone
-                    break
-            marker = hosted_zone_response.get("NextMarker") if hosted_zone_response.get("IsTruncated") else None
+#     print(f"old_zone = {old_zone}")
+#     if not old_zone and hosted_zone_response.get("IsTruncated"):
+#         marker = hosted_zone_response.get("NextMarker")
+#         while marker and not old_zone:
+#             hosted_zone_response = route53.list_hosted_zones(Marker=marker)
+#             for zone in hosted_zone_response.get("HostedZones"):
+#                 if domain.endswith(zone['Name'][-1]):
+#                     old_zone = zone
+#                     break
+#             marker = hosted_zone_response.get("NextMarker") if hosted_zone_response.get("IsTruncated") else None
      
-    if old_zone:
+#     if old_zone:
 
-        response = route53.list_resource_record_sets(
-            HostedZoneId=old_zone['Id'],
-            StartRecordName=domain
-            )
-        print(f"Record Set Response {response}")
-        for record_set in response.get("ResourceRecordSets"):
-            if record_set.get("Name")[:-1] == domain and record_set.get("Type") == record_type:
-                found_set = record_set
-                break
-    if found_set:
-        found_set['Name'] = found_set['Name'][:-1]
-        if found_set.get("AliasTarget").get("DNSName"):
-            found_set['AliasTarget']['DNSName'] = found_set['AliasTarget']['DNSName'][:-1]
-        eh.add_log("Found Matching Record", {"set": found_set, "domain": domain})
-    else:
-        eh.add_log("No Matching Record", {"domain": domain})
+        
+#     if found_set:
+#         found_set['Name'] = found_set['Name'][:-1]
+#         if found_set.get("AliasTarget").get("DNSName"):
+#             found_set['AliasTarget']['DNSName'] = found_set['AliasTarget']['DNSName'][:-1]
+#         eh.add_log("Found Matching Record", {"set": found_set, "domain": domain})
+#     else:
+#         eh.add_log("No Matching Record", {"domain": domain})
     
-    return found_set, old_zone
+#     return found_set, old_zone
 
 def gen_route53_link(hosted_zone_id):
     return f"https://console.aws.amazon.com/route53/v2/hostedzones#ListRecordSets/{hosted_zone_id}"
